b8b2b8f6c7b53842c73dea3e3abd3ffe7e360db9
Jason <jason@fit2cloud.com>
2015-07-14 16:11:23 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ rm -rf maven-metadata.xml
 wget http://repository.fit2cloud.com/content/repositories/fit2cloud/com/fit2cloud/samples/wordpress-devops-demo/maven-metadata.xml
 
 version=`grep "\<version\>" maven-metadata.xml | tail -n 1 | awk -F">" '{print $2}' | awk -F"<" '{print $1}'`
+/usr/bin/osscmd put maven-metadata.xml oss://f2c-sample-apps/wordpress-devops-demo/maven-metadata.xml
 
 echo version=$version
 
@@ -13,7 +14,6 @@ rm -rf *.zip
 wget http://repository.fit2cloud.com/content/repositories/fit2cloud/com/fit2cloud/samples/wordpress-devops-demo/$version/wordpress-devops-demo-$version-bin.zip
 
 /usr/bin/osscmd put wordpress-devops-demo-$version-bin.zip oss://f2c-sample-apps/wordpress-devops-demo/wordpress-devops-demo-$version-bin.zip
-/usr/bin/osscmd put maven-metadata.xml oss://f2c-sample-apps/wordpress-devops-demo/maven-metadata.xml
 
 rm -rf *.md5
 wget http://repository.fit2cloud.com/content/repositories/fit2cloud/com/fit2cloud/samples/wordpress-devops-demo/$version/wordpress-devops-demo-$version-bin.zip.md5
